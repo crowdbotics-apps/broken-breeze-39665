795b2f9c017ac597ab86202aee2ddd577f3c5041
Crowdbotics <team@crowdbotics.com>
2023-04-02 19:52:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-breeze-39665",
+  url: "https://broken-breeze-39665.botics.co",
+  api: "https://broken-breeze-39665.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
